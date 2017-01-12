88197eb311233ae445238382e1603af470d29ba0
jediazzi <jefferson_diazzi@hotmail.com>
2017-01-11 23:10:41 -0300

    teste

--- web/index.php
+++ web/index.php
@@ -1,1 +1,32 @@
-<?php echo "Olá mundo"; ?>
+<?php
+
+  class User {
+
+    public $id;
+    public $username;
+    public $email;
+    public $password;
+
+    public function __construct() {
+
+    }
+
+    public function register() {
+      echo "Usuário registrado!";
+    }
+
+    public function login($username, $password) {
+      $this->username = $username;
+      $this->password = $password;
+      $this->auto_user($username, $password);
+    }
+
+    public function auth_user() {
+      echo $username . " foi autenticado";
+    }
+
+  }
+
+$User = new User;
+
+$User->login('diazzi','1234');
